6a6ee8ab5e4b0f1e86342b269a801f1b18801778
Paras prajapat <parasprajapat8th.gmail.com>
2024-08-18 12:47:34 +0530

    first commit

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const pool = createPool({
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: false }));
 
 
